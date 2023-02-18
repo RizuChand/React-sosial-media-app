5c3c644993668e4d2d1d7e89c8313528cd1fbfa9
Chandra Rizaldy <dragoriza123@gmail.com>
2023-02-18 13:01:49 +0700

    like function

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,45 +1,44 @@
-import React from "react";
-import './post.css'
-import {MoreVert} from '@mui/icons-material'
+import "./post.css";
+import { MoreVert } from "@mui/icons-material";
 import { users } from "../../dummyData";
+import { useState } from "react";
 
-export default function Post ({post}) {
-    // const [like, setLike] = useState(post.likes);
-    return (
-        <div className="post">
-            <div className="postWrapper">
-                
-                <div className="postTop">
-                    <div className="postTopLeft">
-                        <img className="postProfile" src={users.filter((e) => e.id === post.userId)[0].profilePicture} alt="" />
-                        <span className="postUsername">
+export default function Post({ post }) {
+  const [likes, setLikes] = useState(post.likes);
+  const [isLiked, setLiked] = useState(false);
 
-                            {users.filter((e) => e.id === post.userId)[0].username}
-                            
-                            </span>
-                        <span className="postDate">{post.dates}</span>   
-                    </div>
-                    <div className="postTopRight">
-
-                        <MoreVert/>
-                        
-                    </div>
-                </div>
-                <div className="postCenter">
-                    <span className="postText">{post?.desc}</span>
-                    <img className="postImg" src={post.photo} alt="" />
-                </div>
-                <div className="postBottom">
-                    <div className="postBottomLeft">
-                        <img className="resIcon" src="/assets/like.png" alt="" />
-                        <img className="resIcon" src="/assets/heart.png" alt="" />
-                        <span className="postResCounter">{post.likes} people like it</span>
-                    </div>
-                    <div className="postBottomRight">
-                        <span className="postTextComment">{post.comments} comments</span>
-                    </div>
-                </div>
-            </div>
+  const likeHandler = () => {
+    setLikes(isLiked ? likes + 1 : likes - 1);
+    setLiked(!isLiked);
+  };
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img className="postProfile" src={users.filter((e) => e.id === post.userId)[0].profilePicture} alt="" />
+            <span className="postUsername">{users.filter((e) => e.id === post.userId)[0].username}</span>
+            <span className="postDate">{post.dates}</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
         </div>
-    )
+        <div className="postCenter">
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="resIcon" src="/assets/like.png" onClick={likeHandler} alt="" />
+            <img className="resIcon" src="/assets/heart.png" onClick={likeHandler} alt="" />
+            <span className="postResCounter">{likes} people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postTextComment">{post.comments} comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import "./rightbar.css";
 import { users } from "../../dummyData";
 import Online from "../online/Online";
-import Share from "../share/Share";
 
 export default function Rightbar() {
   return (
-  <div className="rightbar">
-    <div className="rightbarWrapper">
-      <div className="birthdayContainer">
-        <img className="birthdayImg" src="/assets/gift.png" alt="" />
-        <span className="birthdayText"> <b>Manghuang</b> and <b>3 other friends</b> have birthday today </span>
-      </div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="/assets/gift.png" alt="" />
+          <span className="birthdayText">
+            {" "}
+            <b>Manghuang</b> and <b>3 other friends</b> have birthday today{" "}
+          </span>
+        </div>
         <img className="rightbarAd" src="/assets/ad.png" alt="" />
         <h4 className="rightbarTittle">Friends Online</h4>
         <ul className="righbarFriendList">
-          {users.map(e=>(
+          {users.map((e) => (
             <Online key={e.id} user={e} />
           ))}
         </ul>
-
-        
+      </div>
     </div>
-  </div>)
+  );
 }
